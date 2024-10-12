7d8b61388e7b63dbe4bb31857ec10600cd9e048d
Umesh <97531471+umeshyadav7988@users.noreply.github.com>
2024-10-12 17:42:40 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.formRef = React.createRef();
+    this.state = {
+      isSubmitted: false,
+    };
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const form = this.formRef.current;
+    const name = form.elements['name'].value;
+    const email = form.elements['email'].value;
+    const message = form.elements['message'].value;
+
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Message:', message);
+
+    this.setState({ isSubmitted: true });
+    
+    form.reset();
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <form ref={this.formRef} onSubmit={this.handleSubmit}>
+          <div>
+            <label>Name</label>
+            <input type="text" name="name" required />
+          </div>
+          <div>
+            <label>Email</label>
+            <input type="email" name="email" required />
+          </div>
+          <div>
+            <label>Message</label>
+            <textarea name="message" required />
+          </div>
+          <button type="submit">Send Message</button>
+        </form>
+        {this.state.isSubmitted && <p>Message Sent to CultureLinkr</p>}
+      </div>
+    );
+  }
 }
 
 export default App;
